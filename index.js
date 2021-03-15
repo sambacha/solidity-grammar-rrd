c0ca6098c0e8fb723f930d601573aacd6b2596da
sam bacha <sam@freighttrust.com>
2021-03-14 21:23:02 -0700

    chore(js): use strict

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// @file railroad grammar generator
+// @license MIT
+'use strict';
 var fs = require('fs');
 
 const { parseEbnf, createDocumentation } = require("ebnf2railroad");
@@ -5,3 +8,4 @@ const { parseEbnf, createDocumentation } = require("ebnf2railroad");
 var ebnf = fs.readFileSync('solidity.ebnf','utf8');
 const ast = parseEbnf(ebnf); // can throw parse error exceptions
 const htmlOutput = createDocumentation(ast);
+// @exports html {solidity.ebnf}
